921eb42bc4899cc7cdce6c49417ec2d3634e56f8
TechNik <aniket.97.mane@gmail.com>
2022-09-19 13:26:34 +0530

    Added projects resources route

--- lib/nova_web/router.ex
+++ lib/nova_web/router.ex
@@ -18,6 +18,7 @@ defmodule NovaWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    resources "/projects", ProjectController
   end
 
   # Other scopes may use custom stacks.
