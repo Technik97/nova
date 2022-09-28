007270881a9ea0754f5572b8b46aaea40570c148
TechNik <aniket.97.mane@gmail.com>
2022-09-28 21:41:55 +0530

    Implemented many to many association between Users and Projects

--- lib/nova/workspaces/project.ex
+++ lib/nova/workspaces/project.ex
@@ -3,10 +3,13 @@ defmodule Nova.Workspaces.Project do
   import Ecto.Changeset
 
   alias __MODULE__
+  alias Nova.Repo
 
   schema "projects" do
     field :description, :string
     field :name, :string
+    many_to_many :users, Nova.Accounts.User,
+      join_through: Nova.Workspaces.ProjectUser
 
     timestamps()
   end
@@ -17,7 +20,9 @@ defmodule Nova.Workspaces.Project do
   @spec changeset({map, map}| %{:__struct__ => atom | %{:__changeset__ => map, optional(any) => any},optional(atom) => any},:invalid | %{optional(:__struct__) => none, optional(atom | binary) => any}) :: Ecto.Changeset.t()
   def changeset(project \\ %Project{}, attrs) do
     project
+    |> Repo.preload(:users)
     |> cast(attrs, @fields)
+    |> cast_assoc(:users)
     |> validate_required(@required_fields)
   end
 end
